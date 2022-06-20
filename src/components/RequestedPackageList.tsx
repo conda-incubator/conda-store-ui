7cb3cf2de0682575770d492898a071bcfc15ca3e
ostojics <ostojic.dev@gmail.com>
2022-06-20 12:00:37 +0200

    style: add scrollbar styling

--- src/components/RequestedPackageList.tsx
+++ src/components/RequestedPackageList.tsx
@@ -60,6 +60,17 @@ const RequestedPackageList = ({ packageList }: IRequestedPackageListProps) => {
           padding: "11px 40px",
           maxHeight: "90px",
           overflowY: "scroll",
+          "&::-webkit-scrollbar": {
+            width: "15px",
+          },
+          "&::-webkit-scrollbar-track": {
+            backgroundColor: "transparent",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "#C4C4C4",
+            borderRadius: "10px",
+            border: "1px solid #666666",
+          },
         }}
       >
         {filteredPackageList.map((item, index) => (
