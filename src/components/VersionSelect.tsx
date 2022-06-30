e29b396a4b4f984f4634a140c3cd0791e93fef8f
ostojics <ostojic.dev@gmail.com>
2022-06-30 13:40:01 +0200

    chore: use theme

--- src/components/VersionSelect.tsx
+++ src/components/VersionSelect.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import useTheme from "@mui/material/styles/useTheme";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 interface IVersionSelectProps {
@@ -8,6 +9,7 @@ interface IVersionSelectProps {
 }
 
 const VersionSelect = ({ version }: IVersionSelectProps) => {
+  const { palette } = useTheme();
   const versions = ["1.0.0", "5.8.0", "15.0.0", "20.13.15"];
 
   if (version) versions.push(version);
@@ -20,7 +22,7 @@ const VersionSelect = ({ version }: IVersionSelectProps) => {
           sx={{
             backgroundColor: "#ECECEC",
             height: "37px",
-            borderLeft: "2px solid  #C4C4C4",
+            borderLeft: `2px solid  ${palette.primary.main}`,
           }}
         />
       )}
