42c8f979bc6aaae0d7899fcb8b24d7e2ee268c52
ostojics <ostojic.dev@gmail.com>
2022-07-04 11:41:06 +0200

    chore: display channels list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import "../style/index.css";
 import ChannelsEdit from "./components/Channels/ChannelsEdit";
+import ChannelsList from "./components/Channels/ChannelsList";
 
 const App = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <ChannelsList
+        channelList={["conda-store", "default", "conda forge"]}
+        listHeight={213}
+      />
       <ChannelsEdit
         channelsList={["conda-store", "default", "conda forge"]}
         listHeight={170}
