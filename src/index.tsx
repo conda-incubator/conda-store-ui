7db1698790756d3e6560a44bc361c107ee9581ba
ostojics <ostojic.dev@gmail.com>
2022-07-13 11:56:41 +0200

    chore: update import

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./App";
-import { store } from "./store/store";
+import { store } from "./store";
 import { theme } from "./theme";
 
 const container = document.createElement("div");
