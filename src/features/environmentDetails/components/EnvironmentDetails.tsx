33c8827cbd457842d3a1b556a6952ec8e50acf82
ostojics <ostojic.dev@gmail.com>
2022-07-25 10:32:56 +0200

    feat: add layout

--- src/features/environmentDetails/components/EnvironmentDetails.tsx
+++ src/features/environmentDetails/components/EnvironmentDetails.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { Specification } from "./Specification";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useAppSelector } from "src/hooks";
+import { StyledButtonPrimary } from "src/styles";
 
 export const EnvironmentDetails = () => {
+  const { mode } = useAppSelector(state => state.environmentDetails);
+
   return (
-    <div>
-      <Specification />
-    </div>
+    <Box sx={{ border: "1px solid #000", padding: "18px 12px" }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: "19px"
+        }}
+      >
+        <Typography sx={{ fontSize: "24px", color: "#000" }}>
+          Machine Learning
+        </Typography>
+        <StyledButtonPrimary>Edit</StyledButtonPrimary>
+      </Box>
+      <Box sx={{ marginBottom: "30px" }}>
+        <Typography>Environment Metadata</Typography>
+      </Box>
+      <Box sx={{ marginBottom: "30px" }}>
+        <Specification />
+      </Box>
+      {mode === "read-only" && (
+        <Box>
+          <Typography>Logs and artifacts</Typography>
+        </Box>
+      )}
+    </Box>
   );
 };
