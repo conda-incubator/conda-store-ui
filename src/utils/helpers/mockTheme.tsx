5c2eebca3cc004811ece3013f320f48333747ed1
telamonian <telamonian@users.noreply.github.com>
2022-07-27 09:37:50 -0400

    post-rebase cleanup

--- src/utils/helpers/mockTheme.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "../../theme";
-
-export const mockTheme = (children: any) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
