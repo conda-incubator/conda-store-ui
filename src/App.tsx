cf44e2c5ff707e1508ebab2436bfdabf59c4f9c1
ostojics <ostojic.dev@gmail.com>
2022-07-27 16:46:07 +0200

    chore: add basic requests

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import "../style/index.css";
 import { EnvironmentDetails } from "src/features/environmentDetails";
+import { useGetBuildQuery, useGetVersionQuery } from "./features/api";
 
 export const App = () => {
+  // tried creating a basic request using fetch API, still the same error
+
+  const { data } = useGetBuildQuery();
+  const { data: versionResponse } = useGetVersionQuery();
+
+  console.log(data);
+  console.log(versionResponse);
+
   return (
     <>
       <Typography sx={{ fontSize: "25px", textAlign: "center" }}>
--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,12 +1,30 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "" // add base API url when we start working with it
+  baseUrl: "http://localhost:5000/conda-store/api/v1",
+  // token add from http://localhost:5000/conda-store/user/
+  prepareHeaders: headers => {
+    headers.set(
+      "Authorization",
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTkwMTkyMTksInByaW1hcnlfbmFtZXNwYWNlIjoiYWRtaW4iLCJyb2xlX2JpbmRpbmdzIjp7ImRlZmF1bHQvKiI6WyJ2aWV3ZXIiXSwiZmlsZXN5c3RlbS8qIjpbInZpZXdlciJdLCIqLyoiOlsiYWRtaW4iXX19.mHIZeDuKkY7aO4Bq8QFnaL5wZFYE2E_PK_9cj9OgTNY"
+    );
+
+    return headers;
+  }
 });
 
 export const apiSlice = createApi({
   reducerPath: "api",
   tagTypes: [], // add tag types when needed to utilize the cache, see https://redux-toolkit.js.org/rtk-query/api/createApi#tagtypes
-  endpoints: () => ({}),
+  endpoints: builder => ({
+    getBuild: builder.query<any, void>({
+      query: () => "/build/1"
+    }),
+    getVersion: builder.query<any, void>({
+      query: () => "/"
+    })
+  }),
   baseQuery
 });
+
+export const { useGetBuildQuery, useGetVersionQuery } = apiSlice;
