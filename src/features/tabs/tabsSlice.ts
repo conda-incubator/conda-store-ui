9528b7a7c7ca854ce6e35ca2dcd43a56915c5bce
ostojics <ostojic.dev@gmail.com>
2022-08-03 09:26:50 +0200

    chore: handle edge case

--- src/features/tabs/tabsSlice.ts
+++ src/features/tabs/tabsSlice.ts
@@ -18,10 +18,15 @@ export const tabsSlice = createSlice({
   initialState,
   reducers: {
     environmentOpened: (state, action: PayloadAction<Environment>) => {
-      // handle when the same env is opened multiple times
-      state.selectedEnvironment = action.payload;
-      state.value = action.payload.id;
-      state.selectedEnvironments.push(action.payload);
+      const environments = state.selectedEnvironments;
+      const openedEnvironment = action.payload;
+
+      state.selectedEnvironment = openedEnvironment;
+      state.value = openedEnvironment.id;
+
+      if (!environments.some(env => env.id === openedEnvironment.id)) {
+        state.selectedEnvironments.push(openedEnvironment);
+      }
     },
     environmentClosed: (state, action: PayloadAction<number>) => {
       const currentlySelectedEnvironment = state.selectedEnvironment;
@@ -38,16 +43,13 @@ export const tabsSlice = createSlice({
         ? state.selectedEnvironments[0].id
         : 0;
     },
-    environmentSelected: (state, action: PayloadAction<Environment>) => {
-      state.selectedEnvironment = action.payload;
-      state.value = action.payload.id;
-    },
     valueChanged: (state, action: PayloadAction<number>) => {
       const environment = state.selectedEnvironments.find(
         env => env.id === action.payload
       );
 
       state.value = action.payload;
+
       if (environment) {
         state.selectedEnvironment = environment;
       }
@@ -55,9 +57,5 @@ export const tabsSlice = createSlice({
   }
 });
 
-export const {
-  environmentOpened,
-  environmentClosed,
-  valueChanged,
-  environmentSelected
-} = tabsSlice.actions;
+export const { environmentOpened, environmentClosed, valueChanged } =
+  tabsSlice.actions;
