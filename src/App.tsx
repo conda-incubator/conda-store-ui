e93c3cc03acfb76e79a35f051d9199f8559d54dc
Juan José González <juanjose.gonzalez@morningstar.com>
2022-08-04 12:56:10 -0500

    use hook to call the api

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import "../style/index.css";
 import { ArtifactsList } from "src/features/artifacts";
+import { useGetEnviromentQuery } from "src/features/metadata/metadataApiSlice";
 
 export const App = () => {
+  const result = useGetEnviromentQuery(1); // rejected
+  console.log(result);
+
   const artifactsList = [
     {
       name: "Link to lockfile",
