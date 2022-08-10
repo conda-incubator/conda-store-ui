3ada963dfce978237057910b1f0c9274576b8073
sahn1 <seohyun.ahn@morningstar.com>
2022-08-10 15:44:28 -0500

    Move login button under EnvSearch and change artifacts url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Link as RouterLink } from "react-router-dom";
-import Link from "@mui/material/Link";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Routes } from "react-router";
 import "../style/index.css";
 import { PageLayout } from "./layouts";
@@ -9,9 +8,6 @@ import { LoginPage } from "./features/login";
 export const App = () => {
   return (
     <Router>
-      <Link component={RouterLink} to="/login" color="primary">
-        Sign in
-      </Link>
       <Routes>
         <Route path="/" element={<PageLayout />} />
         <Route path="/login" element={<LoginPage />} />
