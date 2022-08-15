ad35cfc0b957d5b2bdace3848d29e4fedaa39738
ostojics <ostojic.dev@gmail.com>
2022-08-15 09:35:47 +0200

    feat: add persistent data instead of mock data

--- src/features/environments/components/Environments.tsx
+++ src/features/environments/components/Environments.tsx
@@ -1,50 +1,35 @@
-import React, { memo, useMemo, useState } from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import useTheme from "@mui/material/styles/useTheme";
 import EnvironmentsList from "./EnvironmentsList";
-import { Environment } from "src/common/models";
 import { debounce } from "lodash";
 import { EnvironmentsSearch } from "./EnvironmentsSearch";
+import { useFetchEnvironmentsQuery } from "../environmentsApiSlice";
 
-export interface IEnvironmentsProps {
-  /**
-   * @param list environments list
-   */
-  list: Environment[];
-}
-
-const BaseEnvironments = ({ list }: IEnvironmentsProps) => {
+export const Environments = () => {
   const [search, setSearch] = useState("");
   const {
     palette: { primary }
   } = useTheme();
 
+  const { data } = useFetchEnvironmentsQuery({
+    page: 1,
+    size: 100,
+    search
+  });
+
   const handleChange = debounce((value: string) => {
     setSearch(value);
   }, 500);
-
-  const filteredList = useMemo(
-    () => list.filter(item => item.name.includes(search)),
-    [search]
-  );
 
   return (
     <Box sx={{ width: "313px", border: `1px solid ${primary.main}` }}>
       <Box sx={{ borderBottom: `1px solid ${primary.main}` }}>
         <EnvironmentsSearch onChange={e => handleChange(e.target.value)} />
       </Box>
-      <EnvironmentsList environmentsList={filteredList} />
+      <Box sx={{ height: "550px" }}>
+        {data && <EnvironmentsList environmentsList={data.data} />}
+      </Box>
     </Box>
   );
 };
-
-// memoize the component, rerender only when the arrays from props are different
-// use JSON.stringify to turn arrays into string for === check to work propertly
-const compareProps = (
-  prevProps: IEnvironmentsProps,
-  nextProps: IEnvironmentsProps
-) => {
-  return JSON.stringify(prevProps.list) === JSON.stringify(nextProps.list);
-};
-
-export const Environments = memo(BaseEnvironments, compareProps);
