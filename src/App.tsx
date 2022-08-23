63dc15ef7647bbff769f6789c96b33f513b4a2a0
ostojics <ostojic.dev@gmail.com>
2022-08-23 09:54:53 +0200

    refactor: change file names

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Routes } from "react-router";
 import "../style/index.css";
 import { PageLayout } from "./layouts";
-import { LoginPage } from "./features/login";
+import { LoginPage } from "./features/auth";
 
 export const App = () => {
   return (
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-  name: "login",
+  name: "auth",
   initialState: {},
   reducers: {}
 });
