c81b33177b8edd9acd0d97cb183042f8d9178b4f
Juan José González <juanjose.gonzalez@morningstar.com>
2022-08-29 09:15:13 -0500

    use yaml package to parse yaml files

--- src/features/yamlEditor/components/editor.tsx
+++ src/features/yamlEditor/components/editor.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { StreamLanguage } from "@codemirror/language";
-import { yaml } from "@codemirror/legacy-modes/mode/yaml";
+import { yaml as yamlLanguage } from "@codemirror/legacy-modes/mode/yaml";
+import { parse, stringify } from "yaml";
 
 export interface ICodeEditor {
   code: any;
 }
 
 export const CodeEditor = ({ code }: ICodeEditor) => {
+  const test = (e: string) => {
+    console.log(parse(e));
+  };
   return (
     <CodeMirror
       value={code}
       height="200px"
-      extensions={[StreamLanguage.define(yaml)]}
+      extensions={[StreamLanguage.define(yamlLanguage)]}
+      onChange={e => test(e)}
     />
   );
 };
