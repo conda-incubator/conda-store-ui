75599bfd065ee2f9ce5e4f0245d7f31f2a08fd6c
telamonian <telamonian@users.noreply.github.com>
2022-09-20 09:18:17 -0400

    fixup npm package and build

--- src/features/metadata/components/EnvMetadata.tsx
+++ src/features/metadata/components/EnvMetadata.tsx
@@ -1,14 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useState } from "react";
+import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
 import useTheme from "@mui/material/styles/useTheme";
+import React from "react";
+
+import { useGetEnviromentsQuery } from "src/features/metadata";
 import { EnvBuilds, Description } from "src/features/metadata/components";
 import { StyledBox } from "src/styles";
-import { useGetEnviromentsQuery } from "src/features/metadata";
 
 export enum EnvironmentDetailsModes {
   "CREATE" = "create",
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+export * from "./App";
+export { store } from "./store";
+export { theme } from "./theme";
+
 import { ThemeProvider } from "@mui/material";
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+
 import { App } from "./App";
 import { store } from "./store";
 import { theme } from "./theme";
@@ -12,13 +17,19 @@ const body = document.querySelector("body");
 container.setAttribute("id", "app");
 body?.appendChild(container);
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!);
-
-root.render(
-  <Provider store={store}>
+export const AppExample = () => {
+  return (
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </Provider>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+
+root.render(
+  <AppExample/>
 );
