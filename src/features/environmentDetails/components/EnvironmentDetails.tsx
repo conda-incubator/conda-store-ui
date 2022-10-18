ce575bdfbceca6506a76247fe87886e1f62a2a8b
Juan José González <juanjose.gonzalez@morningstar.com>
2022-10-18 12:59:11 -0500

    fix minor fixes

--- src/features/environmentDetails/components/EnvironmentDetails.tsx
+++ src/features/environmentDetails/components/EnvironmentDetails.tsx
@@ -31,6 +31,8 @@ interface IUpdateEnvironmentArgs {
   channels: string[];
 }
 
+const REFRESH_POLLING_INTERVAL = 2000;
+
 export const EnvironmentDetails = ({
   environmentNotification
 }: IEnvDetails) => {
@@ -40,14 +42,11 @@ export const EnvironmentDetails = ({
   const { page } = useAppSelector(state => state.dependencies);
   const { selectedEnvironment } = useAppSelector(state => state.tabs);
   const { currentBuild } = useAppSelector(state => state.enviroments);
-
-  // const [enviromentBuilds, setEnviromentBuilds] = useState<any>([]);
   const [name, setName] = useState(selectedEnvironment?.name || "");
   const [descriptionIsUpdated, setDescriptionIsUpdated] = useState(false);
   const [description, setDescription] = useState(
     selectedEnvironment ? selectedEnvironment.description : undefined
   );
-  const [envIsUpdated, setEnvIsUpdated] = useState(false);
   const [error, setError] = useState({
     message: "",
     visible: false
@@ -55,7 +54,7 @@ export const EnvironmentDetails = ({
 
   const [createOrUpdate] = useCreateOrUpdateMutation();
   useGetEnviromentBuildsQuery(selectedEnvironment, {
-    pollingInterval: 2000
+    pollingInterval: REFRESH_POLLING_INTERVAL
   });
 
   const { isFetching } = useGetBuildQuery(currentBuild.id, {
@@ -95,11 +94,11 @@ export const EnvironmentDetails = ({
       dispatch(modeChanged(EnvironmentDetailsModes.READ));
       dispatch(updatePackages(code.dependencies));
       dispatch(updateChannels(code.channels));
+      setDescription(description);
       environmentNotification({
         show: true,
         description: `${name} environment has been updated`
       });
-      setEnvIsUpdated(true);
     } catch (e) {
       setError({
         message: e?.data?.message ?? e.error ?? e.status,
@@ -112,7 +111,7 @@ export const EnvironmentDetails = ({
     setName(selectedEnvironment?.name || "");
     setDescription(selectedEnvironment?.description || "");
     setDescriptionIsUpdated(false);
-  }, [selectedEnvironment, envIsUpdated]);
+  }, [selectedEnvironment]);
 
   return (
     <Box sx={{ padding: "14px 12px" }}>
