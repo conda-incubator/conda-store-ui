d57bc26069761810d5e0ae939048461601035f83
Juan José González <juanjose.gonzalez@morningstar.com>
2022-11-17 10:03:44 -0500

    Jupyter settings can be used to dynamically change the app's style. 

--- src/preferences.tsx
+++ src/preferences.tsx
@@ -5,6 +5,7 @@ export interface IPreferences {
   authMethod: "cookie" | "token";
   authToken: string;
   loginUrl: string;
+  appStyle: string;
 }
 
 export const prefDefault: Readonly<IPreferences> = {
@@ -15,7 +16,8 @@ export const prefDefault: Readonly<IPreferences> = {
   authToken: process.env.REACT_APP_AUTH_TOKEN ?? "",
   loginUrl:
     process.env.REACT_APP_LOGIN_PAGE_URL ??
-    "http://localhost:5000/conda-store/login?next"
+    "http://localhost:5000/conda-store/login?next",
+  appStyle: process.env.REACT_APP_STYLE_TYPE ?? "grayscale"
 };
 
 export class Preferences implements IPreferences {
@@ -39,17 +41,23 @@ export class Preferences implements IPreferences {
     return this._loginUrl;
   }
 
+  get appStyle() {
+    return this._appStyle;
+  }
+
   set(pref: IPreferences) {
     this._apiUrl = pref.apiUrl;
     this._authMethod = pref.authMethod;
     this._authToken = pref.authToken;
     this._loginUrl = pref.loginUrl;
+    this._appStyle = pref.appStyle;
   }
 
   private _apiUrl: IPreferences["apiUrl"];
   private _authMethod: IPreferences["authMethod"];
   private _authToken: IPreferences["authToken"];
   private _loginUrl: IPreferences["loginUrl"];
+  private _appStyle: IPreferences["appStyle"];
 }
 
 export const prefGlobal = new Preferences();
