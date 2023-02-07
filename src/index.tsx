701b571ce8c4fd8568ab95b89042f4c6c9f11fec
telamonian <telamonian@users.noreply.github.com>
2023-02-07 10:29:49 -0500

    added top level `createApp`; index.html webpack template now functions correctly

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,15 @@ export * from "./App";
 export { IPreferences } from "./preferences";
 export { store } from "./store";
 export { theme, themeDecorator } from "./theme";
+
+import React from "react";
+import { App } from "./App";
+import { createRoot } from "react-dom/client";
+
+export function createApp(elem: HTMLElement) {
+  // const root = ReactDOM.createRoot(document.getElementById('root'));
+  const root = createRoot(elem);
+  root.render(React.createElement(App, null, null));
+
+  return root;
+}
