28a77f8afac7dbafe9fdf0a48565b761616ff542
Stephannie Jimenez <steff456@hotmail.com>
2023-10-20 15:39:58 -0500

    Add new design to links

--- src/features/artifacts/components/ArtifactsItem.tsx
+++ src/features/artifacts/components/ArtifactsItem.tsx
@@ -1,6 +1,8 @@
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import React from "react";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { useTheme } from "@mui/material/styles";
 
 import { Artifact } from "../../../common/models";
 import { PrefContext } from "../../../preferences";
@@ -19,13 +21,36 @@ export const ArtifactItem = ({ artifact }: IArtifactsProps) => {
     ? pref.apiUrl
     : `${window.location.origin}${pref.apiUrl}`;
   const route = new URL(artifact.route, url).toString();
+  const theme = useTheme();
 
   return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        "&:hover": {
+          borderBottom: "1px solid"
+        },
+        "&:focus": {
+          borderBottom: `1px solid ${theme.palette.primary.main}`,
+          backgroundColor: theme.palette.primary[50],
+          color: theme.palette.primary[600]
+        }
+      }}
+    >
+      <OpenInNewIcon />
       <Link
         href={route}
+        color="secondary"
         underline="none"
-        sx={{ color: "#333", fontSize: "13px" }}
+        sx={{
+          fontSize: "14px",
+          marginLeft: "5px",
+          "&:focus": {
+            color: theme.palette.primary[600]
+          }
+        }}
         target="_blank"
       >
         {artifact.name}
