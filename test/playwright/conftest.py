9bcdc8ba5e7329ac7858b4d6ca6652010394d2c5
Kim Pevey <kcpevey@quansight.com>
2023-11-27 13:02:48 -0600

    Add default timeout
    add default timeout

--- test/playwright/conftest.py
+++ test/playwright/conftest.py
@@ -1,5 +1,10 @@
 import pytest
 
+from playwright.sync_api import expect
+
+DEFAULT_TIMEOUT = 30_000  # time in ms
+
+expect.set_options(timeout=DEFAULT_TIMEOUT)
 
 def pytest_addoption(parser):
     parser.addoption("--screenshots", action="store", default="false")
