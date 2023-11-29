05efd3ae3268f6f0fe56d3c425c29db708e10db5
Kim Pevey <kcpevey@quansight.com>
2023-11-29 08:48:42 -0600

    silly mistakes

--- test/playwright/test_ux.py
+++ test/playwright/test_ux.py
@@ -6,8 +6,7 @@ import requests
 import time
 
 import pytest
-from playwright.sync_api import Page
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page, sync_playwright, expect
 import random
 
 
