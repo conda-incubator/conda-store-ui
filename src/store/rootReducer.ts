7a736af2e23da2893fa3169ab71ac15d3eb85254
gabalafou <gabriel@fouasnon.com>
2024-04-09 12:07:07 -0400

    todo comments

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -7,6 +7,7 @@ import { environmentVariablesSlice } from "../features/environmentVariables";
 import { tabsSlice } from "../features/tabs";
 import { enviromentsSlice } from "../features/metadata";
 import { environmentCreateSlice } from "../features/environmentCreate/environmentCreateSlice";
+// TODO rename */reducer.ts files to match */*slice.ts pattern
 import { namespacesSlice } from "../features/namespaces/reducer";
 import { environmentsSlice } from "../features/environments/reducer";
 import { notificationSlice } from "../features/notification/notificationSlice";
@@ -17,6 +18,7 @@ export const rootReducer = {
   requestedPackages: requestedPackagesSlice.reducer,
   environmentVariables: environmentVariablesSlice.reducer,
   tabs: tabsSlice.reducer,
+  // TODO: rename (fix spelling) and consolidate with environments slice
   enviroments: enviromentsSlice.reducer,
   environmentDetails: environmentDetailsSlice.reducer,
   dependencies: dependenciesSlice.reducer,
