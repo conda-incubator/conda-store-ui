4929d0aca8ad302782347b0cd598a4930a75a18c
Nikita Karetnikov <nikita@karetnikov.org>
2024-04-30 03:15:44 +0200

    Remove workarounds that are no longer needed

--- src/features/environmentCreate/components/Specification/SpecificationReadOnly.tsx
+++ src/features/environmentCreate/components/Specification/SpecificationReadOnly.tsx
@@ -34,8 +34,7 @@ export const SpecificationReadOnly = () => {
         />
       </Box>
       <Box sx={{ margiBottom: "30px" }}>
-        {/* TODO: make sure channels are never undefined in the state */}
-        <ChannelsList channelList={channels ?? []} />
+        <ChannelsList channelList={channels} />
       </Box>
     </BlockContainer>
   );
