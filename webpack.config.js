0b26f868e57daed0403f8c49d9b6648973936d29
gabalafou <gabriel@fouasnon.com>
2024-10-29 16:05:56 -0400

    Add React router tests (#430)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2020, conda-store development team
  *
- * This file is distributed under the terms of the BSD 3 Clause license. 
+ * This file is distributed under the terms of the BSD 3 Clause license.
  * The full license can be found in the LICENSE file.
  */
 
@@ -18,7 +18,7 @@ const isProd = process.env.NODE_ENV === "production";
 const ASSET_PATH = isProd ? "" : "/";
 const version = packageJson.version;
 
-// Calculate hash based on content, will be used when generating production 
+// Calculate hash based on content, will be used when generating production
 // bundles
 const cssLoader = {
   loader: "css-loader",
@@ -92,6 +92,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ title: "conda-store" }),
+    new HtmlWebpackPlugin({
+      filename: "memory-router-test.html",
+      template: "test/playwright/memory-router-test.html",
+    }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
     }),
@@ -101,7 +105,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.VERSION': JSON.stringify(version),
     }),
-    // Add comment to generated files indicating the hash and ui version 
+    // Add comment to generated files indicating the hash and ui version
     // this is helpful for vendoring with server
     new webpack.BannerPlugin({
       banner: `file: [file], fullhash:[fullhash] - ui version: ${version}`,
